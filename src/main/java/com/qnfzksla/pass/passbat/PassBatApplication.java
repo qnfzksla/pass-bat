0728a2725e9e9bb17ae001120919eb69f726fe47
sd <p971205@naver.com>
2023-03-09 18:40:40 +0900

    first commit

---
+++ src/main/java/com/qnfzksla/pass/passbat/PassBatApplication.java
@@ -0,0 +1,13 @@
+package com.qnfzksla.pass.passbat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PassBatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PassBatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/qnfzksla/pass/passbat/PassBatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qnfzksla.pass.passbat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PassBatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
